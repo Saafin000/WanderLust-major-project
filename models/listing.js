50ad00799f8205a7dd82ff87a8b245d4f05292d4
Saafin <saafinhasan27@gmail.com>
2025-06-21 14:12:32 +0530

    Add boilerplate, navbar, footer, and style updates for all listings pages

--- models/listing.js
+++ models/listing.js
@@ -10,10 +10,17 @@ const ListingSchema = new Schema({
         type: String,
     },
     image: {
-        type: String,
-        default:"https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?q=80&w=2075&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set: (v) => !v ? "https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?q=80..." : v
+    filename: String,
+    url: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      set: (v) =>
+        v === ""
+          ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+          : v,
     },
+  },
     price: {
         type: Number,
     },
@@ -26,5 +33,5 @@ const ListingSchema = new Schema({
 
 });
 
-const Listing=mongoose.model("Listing",ListingSchema);
-module.exports=Listing;
+const Listing = mongoose.model("Listing", ListingSchema);
+module.exports = Listing;
