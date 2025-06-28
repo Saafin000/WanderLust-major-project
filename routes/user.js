39f3c13ae3bf4f0d9bbf9e80ca35d7737378d9f0
Saafin <saafinhasan27@gmail.com>
2025-06-28 23:33:28 +0530

    Refactor to MVC structure, implement Cloudinary image upload, styled rating UI, and improved routes for listings, reviews, and users

--- routes/user.js
+++ routes/user.js
@@ -5,47 +5,20 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 
-router.get("/signup",(req,res)=>{
-    res.render("users/signup.ejs");
-});
+const userController=require("../controllers/users.js");
 
-router.post("/signup",wrapAsync(async(req,res)=>{
-    try{
-        let {username,email,password}=req.body;
-        const newuser=new User({email,username});
-        const registeredUser=await User.register(newuser,password);
-        console.log(registeredUser);
-        req.login(registeredUser,(err)=>{
-            if(err){
-                return next(err);
-            }
-            req.flash("success","Welcome to WanderLust!");
-            res.redirect(req.session.redirectUrl);
-        });
-    }catch(e){
-        req.flash("error",e.message);
-        res.redirect("/signup");
-    }
-}));
 
-router.get("/login",(req,res)=>{
-    res.render("users/login.ejs");
-});
+router.route("/signup")
+    .get(userController.renderSignupForm)
+    .post(wrapAsync(userController.signup));
 
-router.post("/login",saveRedirectUrl,passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}),async (req,res)=>{
-    req.flash("success","Welcome back to WanderLust!");
-    let redirectUrl=res.locals.redirectUrl || "/listings";
-    res.redirect(redirectUrl);
-});
 
-router.get("/logout",(req,res,next)=>{
-    req.logOut((err)=>{
-        if(err){
-           return next(err);
-        }
-        req.flash("success","You are logged out!");
-        res.redirect("/listings");
-    });
-});
+router.route("/login")
+    .get(userController.renderLoginForm)
+    .post(saveRedirectUrl,passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}),userController.login);
+
+
+
+router.get("/logout",userController.logout);
 
 module.exports=router;
